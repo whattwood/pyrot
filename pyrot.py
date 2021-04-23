53cf67380444797066420560f26efb7a500c88d3
whattwood <82691861+whattwood@users.noreply.github.com>
2021-04-23 00:13:06 -0600

    Update pyrot.py

--- pyrot.py
+++ pyrot.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 # Pi Rotator controller software by VE6WK 2021
 
+# imports and startup software
+import time, io, sys, select, os
+os.system("pigpiod") #start pigpio daemon if it hasn't already
 import pigpio
 pi = pigpio.pi()
-
-import time
-
-import io, sys, select, os
-os.system('cls' if os.name == 'nt' else 'clear')
-os.system("screen -S pyrot1 -dm socat -u -u pty,raw,echo=0,link=/dev/ttyS21 pty,raw,echo=0,link=/dev/ttyS22") #create virtual serial ports on a detached screen
-os.system("screen -S pyrot2 -dm rotctld -m 202 -r /dev/ttyS21") #start hamlib on a detached screen
-
-from encoder import Encoder
 
 import settings
 relay_bus=pi.i2c_open(1,settings.relay_board)
@@ -20,6 +14,13 @@ relay_cw_off=[settings.relay_cw,settings.relay_off]
 relay_ccw_on=[settings.relay_ccw,settings.relay_on]
 relay_ccw_off=[settings.relay_ccw,settings.relay_off]
 
+os.system('cls' if os.name == 'nt' else 'clear')
+os.system("screen -S pyrot1 -dm socat -u -u pty,raw,echo=0,link=/dev/ttyS21 pty,raw,echo=0,link=/dev/ttyS22") #create virtual serial ports on a detached screen
+os.system("screen -S pyrot2 -dm rotctld -m 202 -r /dev/ttyS21") #start hamlib on a detached screen
+
+from encoder import Encoder
+
+# This happens when encoder moves
 def valueChanged(value):
     value=e1.getValue()
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -27,8 +28,10 @@ def valueChanged(value):
     print("Encoder Value:",value)
     print("Press Control-C to exit")
 
-e1 = Encoder(settings.enc_clk, settings.enc_bk, callback=valueChanged) # Run valueChanged when encoder moves
+# Run valueChanged when encoder moves
+e1 = Encoder(settings.enc_clk, settings.enc_bk, callback=valueChanged)
 
+# Initial section of code
 value = e1.getValue()
 os.system('cls' if os.name == 'nt' else 'clear')
 print("GPIO Clk Pin:",settings.enc_clk,", GPIO Bk Pin:",settings.enc_bk)
@@ -36,7 +39,9 @@ print("Encoder Value:",value)
 print("Press Control-C to exit")
 pi.i2c_write_device(relay_bus,relay_cw_off)
 pi.i2c_write_device(relay_bus,relay_ccw_off)
+tty = io.TextIOWrapper(io.FileIO(os.open("/dev/ttyS22",os.O_NOCTTY | os.O_RDWR),"r+"))
 
+# Loop while code runs
 try:
     while value < 5 and value > -5:
             value=e1.getValue()
@@ -49,10 +54,14 @@ try:
             if value < settings.heading:
                     pi.i2c_write_device(relay_bus,relay_ccw_on)
                     pi.i2c_write_device(relay_bus,relay_cw_off)
+            for line in iter(tty.readline, None):
+                print(line.strip())
+                tty.writelines("AZ40.1,0.0 EL0.0,0.0\n")
             time.sleep(1)
 except KeyboardInterrupt:
     pass
 
+# Script shutdown commands
 print("\r\nFinal Encoder Value:",value)
 os.system("screen -S pyrot1 -X quit")
 os.system("screen -S pyrot2 -X quit")
