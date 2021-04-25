bd9692d556db7541a48f1fdad39f8be60f660258
whattwood <82691861+whattwood@users.noreply.github.com>
2021-04-25 08:43:06 -0600

    added comments, changed encoder objects

--- settings.py
+++ settings.py
@@ -1,15 +1,12 @@
 # Settings for pyrot
 
 # GPIO pins for rotary encoder
-enc_clk = 26
-enc_bk = 24
+enc_az = 26 #this pin is used for the azimuth sensor
+enc_el = 24 #this pin will be ussed for elevation sensor in the future
 
 # Relay outputs
 relay_board = 0x11 #i2c relay board address
-relay_cw = 0x01 #i2c relay for clockwise activation
-relay_ccw = 0x02 #i2c relay for counterclockwise
+relay_cw = 0x01 #i2c relay number for clockwise direction
+relay_ccw = 0x02 #i2c relay number for counterclockwise direction
 relay_on = 0x01 #i2c relay on command
 relay_off = 0x00 #i2c relay off command
-
-# Desired encoder heading
-heading = 0
