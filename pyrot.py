089df77939800a575b7c109882645e0f06ae2b53
whattwood <82691861+whattwood@users.noreply.github.com>
2021-05-01 11:32:08 -0600

    change code to expect /etc/pyrot directory

--- pyrot.py
+++ pyrot.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # Pi Rotator controller software by VE6WK 2021 intended for DC rotator with pulse sensor such as Alpha Spid
 
-import time, sys, select, os, serial #load the python modules we'll be using #imports and startup software
+import time, sys, select, os, serial, configparser #load the python modules we'll be using #imports and startup software
 
 import os.path
-from os import path #used for checking whether files exist
+from os import path #used for checking whether files econfigparserxist
 
 os.system("clear") #clear screen
 time.sleep(.1)
@@ -29,12 +29,14 @@ pi = pigpio.pi() #define which Raspberry Pi the pigpio daemon will control - the
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD) #choose the pin numbering scheme
 
-import settings #read our settings.py file and set the objects below
-relay_bus=pi.i2c_open(1,settings.relay_board)
-relay_cw_on=[settings.relay_cw,settings.relay_on]
-relay_cw_off=[settings.relay_cw,settings.relay_off]
-relay_ccw_on=[settings.relay_ccw,settings.relay_on]
-relay_ccw_off=[settings.relay_ccw,settings.relay_off]
+#import settings #read our settings.py file and set the objects below
+config = configparser.ConfigParser()
+config.read("/etc/pyrot/settings.txt")
+relay_bus=pi.i2c_open(1,config.get("pyrotvars","relay_board"))
+relay_cw_on=[config.get("pyrotvars","relay_cw"),config.get("pyrotvars","relay_on")]
+relay_cw_off=[config.get("pyrotvars","relay_cw"),config.get("pyrotvars","relay_off")]
+relay_ccw_on=[config.get("pyrotvars","relay_ccw"),config.get("pyrotvars","relay_on")]
+relay_ccw_off=[config.get("pyrotvars","relay_ccw"),config.get("pyrotvars","relay_off")]
 
 os.system("screen -dmS pyrot1 socat pty,raw,echo=0,link=/dev/ttyS21 pty,raw,echo=0,link=/dev/ttyS22") #create virtual serial ports on a detached screen
 time.sleep(.3)
@@ -94,7 +96,7 @@ def valueChanged(encoderValue): #This happens when encoder moves
             f.write(time.strftime("%Y-%m-%d %H:%M:%S") + " ENCODER ERROR! Motion detected while rotator in unknown motion state!\n")
 
 # Run valueChanged when encoder senses state change
-encoderResult = Encoder(settings.enc_az, callback=valueChanged)
+encoderResult = Encoder(config.get("pyrotvars", "enc_az"), callback=valueChanged)
 
 def pyrotShutdown(): #Script shutdown commands
     print("Final Azimuth Value:",azActual)
@@ -105,7 +107,7 @@ def pyrotShutdown(): #Script shutdown commands
     pi.i2c_write_device(relay_bus,relay_ccw_off)
     pi.i2c_close(relay_bus)
     print(bcolors.DEFAULT + "Cleaned up running processes")
-    filename='/var/spool/pyrot/pyrot_position.txt'
+    filename='/etc/pyrot/pyrot_position.txt'
     fileString = (str(azActual) + ", " + str(elActual) + "\n")
     with open(filename, 'w') as filetowrite:
         filetowrite.write(fileString)
@@ -117,15 +119,15 @@ def pyrotShutdown(): #Script shutdown commands
 
 # Initial section of code, runs once
 os.system('clear')
-print("GPIO AZ Encoder Pin:",settings.enc_az)
+print("GPIO AZ Encoder Pin:",config.get("pyrotvars", "enc_az")
 print("Press Control-C to exit")
 pi.i2c_write_device(relay_bus,relay_cw_off) #turn clockwise relay off
 pi.i2c_write_device(relay_bus,relay_ccw_off) #turn counter-clockwise relay off
 ser = serial.Serial('/dev/ttyS22', 115200, timeout = .01) #connect to serial port piped to rotctld, timeout is important because reading rs232 stalls the whole script
 
-print ("Does /var/spool/pyrot/pyrot_position.txt exist? " + str(path.isfile("/var/spool/pyrot/pyrot_position.txt")))
-if path.isfile("/var/spool/pyrot/pyrot_position.txt") is True:
-    filename="/var/spool/pyrot/pyrot_position.txt"
+print ("Does /etc/pyrot/pyrot_position.txt exist? " + str(path.isfile("/etc/pyrot/pyrot_position.txt")))
+if path.isfile("/etc/pyrot/pyrot_position.txt") is True:
+    filename="/etc/pyrot/pyrot_position.txt"
     with open(filename, 'r') as f:
         readOut = f.read()    # results in a str object
     newstr = ''.join((ch if ch in '0123456789.-e' else ' ') for ch in readOut) #read digits in command string
@@ -134,24 +136,24 @@ if path.isfile("/var/spool/pyrot/pyrot_position.txt") is True:
         if savedPosition[0] != None:
             azActual = (savedPosition[0])
     except:
-        print(bcolors.FAIL + "/var/spool/pyrot/pyrot_position.txt has invalid values!" + bcolors.ENDC)
+        print(bcolors.FAIL + "/etc/pyrot/pyrot_position.txt has invalid values!" + bcolors.ENDC)
         exit()
     try:
         if savedPosition[1] != None:
             elActual = (savedPosition[1])
     except:
-        print(bcolors.FAIL + "/var/spool/pyrot/pyrot_position.txt has invalid values!" + bcolors.ENDC)
+        print(bcolors.FAIL + "/etc/pyrot/pyrot_position.txt has invalid values!" + bcolors.ENDC)
         exit()
 else:
     print(bcolors.FAIL + "Previous position file does not exist, setting values to 0 degrees! Ensure rotator position is North or press Control-C NOW!!!" + bcolors.ENDC)
     print("Waiting 20 seconds for input...")
-    os.system("mkdir /var/spool/pyrot/")
-    os.system("touch /var/spool/pyrot/pyrot_position.txt")
+    os.system("mkdir /etc/pyrot/")
+    os.system("touch /etc/pyrot/pyrot_position.txt")
     azActual = 0.0
     elActual = 0.0
     time.sleep(20)
 
-filenameLog = "/var/spool/pyrot/pyrot_log.txt"
+filenameLog = "/etc/pyrot/pyrot_log.txt"
 print ("Does " + filenameLog + " exist? " + str(path.isfile(filenameLog)))
 if path.isfile("filenameLog") is True:
     with open(filenameLog, 'a') as f:
@@ -239,7 +241,7 @@ try:
                         f.write(time.strftime("%Y-%m-%d %H:%M:%S") + " ERROR! No motion detected while rotator should be turning! Shutting script down.\n")
                     pyrotShutdown() #shut script down
                 if azStableCount == 10:
-                    filename='/var/spool/pyrot/pyrot_position.txt'
+                    filename='/etc/pyrot/pyrot_position.txt'
                     fileString = (str(azActual) + ", " + str(elActual) + "\n")
                     with open(filename, 'w') as filetowrite:
                         filetowrite.write(fileString)
