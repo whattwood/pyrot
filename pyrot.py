1ddeab6f26bba15db4e3906f968e79c639dd7e5f
whattwood <82691861+whattwood@users.noreply.github.com>
2021-05-01 10:20:36 -0600

    adjust text spacing at beginning

--- pyrot.py
+++ pyrot.py
@@ -9,7 +9,7 @@ from os import path #used for checking whether files exist
 os.system("clear") #clear screen
 time.sleep(.1)
 
-print("pyrot is a Python Rotator controller typically used by Ham Radio Operators")
+print("    pyrot is a Python Rotator controller typically used by Ham Radio Operators\n")
 #print("\n    This program is free software: you can redistribute it and/or modify\n    it under the terms of the GNU General Public License as published by\n    the Free Software Foundation, either version 3 of the License, or\n    any later version.")
 #print("\n    This program is distributed in the hope that it will be useful,\n    but WITHOUT ANY WARRANTY; without even the implied warranty of\n    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n    GNU General Public License for more details.")
 print("\n\n   pyrot  Copyright (C) 2021 by VE6WK")
