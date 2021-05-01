952e5abe37fff0924d6e559263f60f644d0209c9
whattwood <82691861+whattwood@users.noreply.github.com>
2021-05-01 13:44:08 -0600

    Update pyrot.py

--- pyrot.py
+++ pyrot.py
@@ -13,7 +13,7 @@ print("\n    pyrot is a Python Rotator controller typically used by Ham Radio Op
 #print("\n    This program is free software: you can redistribute it and/or modify\n    it under the terms of the GNU General Public License as published by\n    the Free Software Foundation, either version 3 of the License, or\n    any later version.")
 #print("\n    This program is distributed in the hope that it will be useful,\n    but WITHOUT ANY WARRANTY; without even the implied warranty of\n    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n    GNU General Public License for more details.")
 print("\n\n   pyrot  Copyright (C) 2021 by VE6WK")
-print("\n\n    This program comes with ABSOLUTELY NO WARRANTY.\n    This is free software, and you are welcome to redistribute it\n    under certain conditions.")
+print("\n\n    This program comes with ABSOLUTELY NO WARRANTY.\n    This is free software, and you are welcome to redistribute it\n    under certain conditions.\n\n")
 
 tmp = os.popen("ps -Af").read() #detect if pigpiod is already running
 processCount = tmp.count('pigpiod')
